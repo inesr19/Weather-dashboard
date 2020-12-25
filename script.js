85fd10fb5e2ae34faa176dbb406c1907c93806e2
Ines Radic <inesradic12314@gmail.com>
2020-12-24 19:58:28 -0800

    add dates and 5 day forecast

--- script.js
+++ script.js
@@ -2,21 +2,23 @@
 function displayInfo(cityName) {
     var cityName = $('.searchBar').val();
     var apiKey = 'd9414f7879ddedfc3df78e947516ecc0'
-    var queryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
+    var queryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey;
 
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function (response) {
         console.log(response)
-        // Grabing coordinates for second ajax call.
+        // Grabing coordinates for second and third ajax call.
         displayIndex(response.coord.lat, response.coord.lon);
-        // Converting kelvins into fahrenheit  and displaying it.
-        var tempFar = (response.main.temp - 273.15) * 1.80 + 32;
-        $('.temp').text('Temperature(F): ' + tempFar.toFixed(2) + '\u00B0');
-        // Display humidity
+        dailyWeather(response.coord.lat, response.coord.lon);
+        // Display city name and icon
+        $('.city').text(response.name);
+        // Display temperature in Fahrenheit
+        $('.temp').text('Temperature(F): ' + response.main.temp + '\u00B0');
+        // Display humidity percentage
         $('.humidity').text('Humidity: ' + response.main.humidity + '%');
-        // Display wind speed
+        // Display wind speed mph
         $('.wind').text('Wind Speed: ' + response.wind.speed + 'mph');
     });
 }
@@ -24,36 +26,60 @@ function displayInfo(cityName) {
 $('.searchBtn').on('click', displayInfo);
 
 
- function displayIndex(lat, lon) {
-     var apiKey = 'd9414f7879ddedfc3df78e947516ecc0'
-     var queryUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=hourly,daily&appid=' + apiKey;
+function displayIndex(lat, lon) {
+    var apiKey = 'd9414f7879ddedfc3df78e947516ecc0'
+    var queryUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=hourly,daily&units=imperial&appid=' + apiKey;
     // AJAX call for UV Index
-     $.ajax({
-         url: queryUrl,
-         method: "GET"
-     }).then(function (response) {
-         console.log(response)
-         $('.uv-index').text('UV Index: ' + response.current.uvi);
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
 
-         if (response.current.uvi <= 2) {
-             $('.uv-index').addClass('low');
-         } else if (response.current.uvi > 3 || response.current.uvi == 8) {
+        $('.uv-index').text('UV Index: ' + response.current.uvi);
+
+        if (response.current.uvi <= 2) {
+            $('.uv-index').addClass('low');
+        } else if (response.current.uvi > 3 || response.current.uvi == 8) {
             $('.uv-index').addClass('moderate');
-         } else if (response.current.uvi > 8) {
+        } else if (response.current.uvi > 8) {
             $('.uv-index').addClass('high');
-         }
-     });
- }
+        }
+    });
+}
 
-//  function dailyWeather() {
-//     var apiKey = 'd9414f7879ddedfc3df78e947516ecc0'
-//     var queryUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=hourly,minutely&appid=' + apiKey;
-//    // AJAX call for UV Index
-//     $.ajax({
-//         url: queryUrl,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log(response)
-//         $('.uv-index').text('UV Index: ' + response.current.uvi);
-//     });
-//  }
+function dailyWeather(lat, lon) {
+    var apiKey = 'd9414f7879ddedfc3df78e947516ecc0'
+    var queryUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=hourly,minutely&units=imperial&appid=' + apiKey;
+    // AJAX call for UV Index
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+
+
+        for (let i = 1; i < 6; i++) {
+            const unixTime = response.daily[i].dt
+            console.log(unixTime);
+            const date = new Date(unixTime * 1000);
+            const newDate = date.toLocaleDateString("en-US");
+
+            const iconId = response.daily[i].weather.icon
+            console.log(iconId)
+            const iconUrl = 'http://openweathermap.org/img/wn/' + iconId + '@2x.png';
+            
+            $('<div>', {
+                text: `${newDate}`
+            }).append($('<img>', {
+                src: iconUrl
+            })).append($('<div>', {
+                text: response.daily[i].temp.day + '\u00B0'
+            })).append($('<div>', {
+                text: response.daily[i].humidity + '%'
+            })).appendTo('.container-day');
+        }
+
+
+
+    });
+}
